3c5c888f0454a35e104a092b1cdaf16ee0651cc9
mateussousadev <sousamateus443@gmail.com>
2024-03-07 21:30:30 -0300

    Style:Front-desing of site

---
+++ src/app/text-mensage/text-mensage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-mensage',
+  templateUrl: './text-mensage.component.html',
+  styleUrls: ['./text-mensage.component.scss']
+})
+export class TextMensageComponent {
+
+}
