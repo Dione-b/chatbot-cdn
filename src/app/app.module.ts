93b27d8c731b214fa30fb7bd49ea4b40d265b4e8
mateussousadev <sousamateus443@gmail.com>
2024-03-11 20:41:17 -0300

    front-end init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { MensageBoxComponent } from './mensage-box/mensage-box.component';
+import { TextMensageComponent } from './text-mensage/text-mensage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWindowComponent,
+    MensageBoxComponent,
+    TextMensageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mensage-box/mensage-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mensage-box',
+  templateUrl: './mensage-box.component.html',
+  styleUrls: ['./mensage-box.component.scss']
+})
+export class MensageBoxComponent {
+
+}
